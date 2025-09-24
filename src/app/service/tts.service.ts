7b3674043f1e9adf0884c8546b429038f2d67689
Ankit Pant <159069425+Ankit-0712@users.noreply.github.com>
2025-09-24 21:38:56 +0530

    feat : created service for text to speech

--- src/app/service/tts.service.ts
+++ src/app/service/tts.service.ts
@@ -5,5 +5,24 @@ import { Injectable } from '@angular/core';
 })
 export class TtsService {
 
-  constructor() { }
+  speak(text: string): Promise<void>{
+    return new Promise((resolve, reject)=>{
+      try{
+        if('speechSynthesis' in window){
+          const utter = new SpeechSynthesisUtterance(text);
+          utter.onend = () => resolve();
+          utter.onerror =(e) => reject(e);
+
+
+          window.speechSynthesis.cancel();
+          window.speechSynthesis.speak(utter);
+        }else{
+          reject(new Error('SpeechSynthesis not supported in this browser.'));
+        }
+      }
+      catch(err){
+        reject(err);
+      }
+    });
+  }
 }
